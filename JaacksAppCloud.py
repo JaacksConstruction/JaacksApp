7ea822186655be0503566227b74f6c5e08d972a2
JaacksConstruction <jaacksconstruction@gmail.com>
2025-07-03 16:35:45 -0500

    Update JaacksAppCloud.py

--- JaacksAppCloud.py
+++ JaacksAppCloud.py
@@ -1392,7 +1392,7 @@ if selected_job_data_for_doc_ig is not None:
 else:
     # Show a placeholder if no job is selected
     st.text_input("Full Address", value="Select a job to see client address", disabled=True)
-        job_description_for_doc_pdf_ig = "N/A"
+    #job_description_for_doc_pdf_ig = "N/A"
 
         if selected_job_name_for_doc_ig != "Select Job" and selected_client_for_doc_ig != "Select Client":
             job_data_query_for_doc_ig = jobs_df[(jobs_df['Client'].astype(str).strip() == selected_client_for_doc_ig.strip()) &
